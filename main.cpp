6237a18f5051612ec3c02e00b7bdfbbc5d8ef9aa
Karim Abdel Hamid <karimah@aucegypt.edu>
2017-12-02 18:56:27 +0200

    File Output

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
 #include <map>
 
-std::string ConvertToMofset(Node *input, float p, float n, float l, bool use_sizing) {
+void ConvertToMofset(Node *input, float p, float n, float l, bool use_sizing, std::string &output, std::string &file_output) {
 	Node *pun = input->DeMorgan();
 	std::cout << "PUN: " << *pun << std::endl;
 	Node *pdn = pun->Invert();
@@ -16,32 +17,47 @@ std::string ConvertToMofset(Node *input, float p, float n, float l, bool use_siz
 
 	unsigned int transistor_count = 0;
 	std::map<std::string, unsigned int> inverters;
-	std::string output;
 	if (use_sizing) {
-		output += "==============\nPUN Network: \n" + pun->Mosfet("Vdd", "y", PUN, inverters, transistor_count, p * l, l);
-		output += "==============\nPDN Network: \n" + pdn->Mosfet("y", "gnd", PDN, inverters, transistor_count, n * l, l);
+		std::string mosfet = pun->Mosfet("Vdd", "y", PUN, inverters, transistor_count, p * l, l);
+		file_output += mosfet;
+		output += "==============\nPUN Network: \n" + mosfet;
+
+		mosfet = pdn->Mosfet("y", "gnd", PDN, inverters, transistor_count, n * l, l);
+		file_output += mosfet;
+		output += "==============\nPDN Network: \n" + mosfet;
 	}
 	else {
-		output += "==============\nPUN Network: \n" + pun->Mosfet("Vdd", "y", PUN, inverters, transistor_count);
-		output += "==============\nPDN Network: \n" + pdn->Mosfet("y", "gnd", PDN, inverters, transistor_count);
+		std::string mosfet = pun->Mosfet("Vdd", "y", PUN, inverters, transistor_count);
+		file_output += mosfet;
+		output += "==============\nPDN Network: \n" + mosfet;
+
+		mosfet = pdn->Mosfet("y", "gnd", PDN, inverters, transistor_count);
+		file_output += mosfet;
+		output += "==============\nPDN Network: \n" + mosfet;
 	}
 
 	if (inverters.size() > 0) {
-		std::string output2 = "Inverters:\n";
+		std::string output2 = "Inverters:\n", file_output_2;
 		if (use_sizing) {
 			for (auto &inverter : inverters) {
-				output2 += std::string("The inverter circuit for ") + inverter.first + " is used " + std::to_string(inverter.second) + " times: \n";
-				output2 += std::string("\tMi") + std::to_string(transistor_count_inverters++) + " wire_inv_" + inverter.first + " " + inverter.first + " Vdd Vdd PMOS [W=" + std::to_string(p * l) + " L=" + std::to_string(l) + "]\n";
-				output2 += std::string("\tMi") + std::to_string(transistor_count_inverters++) + " wire_inv_" + inverter.first + " " + inverter.first + " gnd gnd NMOS [W=" + std::to_string(n * l) + " L=" + std::to_string(l) + "]\n";
+				std::string mosfet = std::string("\tMi") + std::to_string(transistor_count_inverters++) + " wire_inv_" + inverter.first + " " + inverter.first + " Vdd Vdd PMOS [W=" + std::to_string(p * l) + " L=" + std::to_string(l) + "]\n";
+				mosfet += std::string("\tMi") + std::to_string(transistor_count_inverters++) + " wire_inv_" + inverter.first + " " + inverter.first + " gnd gnd NMOS [W=" + std::to_string(n * l) + " L=" + std::to_string(l) + "]\n";
+
+				file_output_2 += mosfet;
+				output2 += std::string("The inverter circuit for ") + inverter.first + " is used " + std::to_string(inverter.second) + " times: \n" + mosfet;
 			}
 		}
 		else {
 			for (auto &inverter : inverters) {
-				output2 += std::string("The inverter circuit for ") + inverter.first + " is used " + std::to_string(inverter.second) + " times: \n";
-				output2 += std::string("\tMi") + std::to_string(transistor_count_inverters++) + " wire_inv_" + inverter.first + " " + inverter.first + " Vdd Vdd PMOS\n";
-				output2 += std::string("\tMi") + std::to_string(transistor_count_inverters++) + " wire_inv_" + inverter.first + " " + inverter.first + " gnd gnd NMOS\n";
+				std::string mosfet = std::string("\tMi") + std::to_string(transistor_count_inverters++) + " wire_inv_" + inverter.first + " " + inverter.first + " Vdd Vdd PMOS\n";
+				mosfet += std::string("\tMi") + std::to_string(transistor_count_inverters++) + " wire_inv_" + inverter.first + " " + inverter.first + " gnd gnd NMOS\n";
+
+				file_output_2 += mosfet;
+				output2 += std::string("The inverter circuit for ") + inverter.first + " is used " + std::to_string(inverter.second) + " times: \n" + mosfet;
 			}
 		}
+
+		file_output = file_output_2 + file_output;
 		output = output2 + output;
 	}
 
@@ -49,8 +65,6 @@ std::string ConvertToMofset(Node *input, float p, float n, float l, bool use_siz
 
 	delete pdn;
 	delete pun;
-	
-	return output;
 }
 
 int main(int argc, const char *argv[]) {
@@ -105,12 +119,28 @@ int main(int argc, const char *argv[]) {
     Node *node_tree = conv.ConvertToNodes(input);
     std::cout << "Input: " << *node_tree << std::endl;
 	bool use_sizing = (n != 0.0f) && (p != 0.0f) && (l != 0.0f);
-    std::cout << ConvertToMofset(node_tree, p, n, l, use_sizing) << std::endl;
-    delete node_tree;
+
+	std::string output, file_output;
+	ConvertToMofset(node_tree, p, n, l, use_sizing, output, file_output);
+	delete node_tree;
+
+    std::cout << output << std::endl;
+
+	std::string path;
+	std::cout << "Please input a path to write to. Type q to quit and prevent writing a file: ";
+	std::cin >> path;
+
+	if (path != "q") {
+		std::ofstream file(path);
+
+		file << file_output;
+
+		file.close();
 
 #ifdef _MSC_VER
-	system("pause");
+		system("pause");
 #endif
+	}
 
     return 0;
 }
